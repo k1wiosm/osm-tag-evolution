e79af7955e0d9259eecc5f8de69a3df6be060dc6
k1wiosm <k1wiosm@gmail.com>
2015-11-28 00:38:12 +0100

    change graph color

--- js/utilities.js
+++ js/utilities.js
@@ -80,12 +80,12 @@ function setupChart() {
 		datasets: [
 			{
 				label: "amenity=drinking_water",
-				fillColor: "rgba(220,220,220,0.2)",
-				strokeColor: "rgba(220,220,220,1)",
-				pointColor: "rgba(220,220,220,1)",
+				fillColor: "rgba(151,187,205,0.2)",
+				strokeColor: "rgba(151,187,205,1)",
+				pointColor: "rgba(151,187,205,1)",
 				pointStrokeColor: "#fff",
 				pointHighlightFill: "#fff",
-				pointHighlightStroke: "rgba(220,220,220,1)",
+				pointHighlightStroke: "rgba(151,187,205,1)",
 				data: []
 			}
 		]
